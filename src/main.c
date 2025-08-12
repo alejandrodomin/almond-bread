787470335a75c6a083626d097114e990870707c5
alejandrodomin <a283le.programmer@gmail.com>
2025-08-12 18:31:03 -0400

    added movement support
    would like to add real time termios support and some visualization with
    more resolution than the terminal but good enough for now

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
-#include <coz.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-int brot_point(double x, double y);
+int brot_point(double real, double imag);
+double coord_trnsfrm(unsigned int index, unsigned int size, double bot_bound, double up_bound);
+char* color(unsigned int count);
 
 int main(void) {
+    // init
     const int width = 100;
     const int height = 50;
     const char* set[height][width];
@@ -12,57 +14,86 @@ int main(void) {
     double real_min = -1.5, real_max = 0.5;
     double imag_min = -1.0, imag_max = 1.0;
 
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            double real = real_min + (x / (double)(width - 1)) * (real_max - real_min);
-            double imag = imag_min + (y / (double)(height - 1)) * (imag_max - imag_min);
+    while (true) {
+        // clear term
+        printf("\033[H");
 
-            switch (brot_point(real, imag)) {
-                case 0:
-                    set[y][x] = "\033[0;40m";  // Black block (in set)
-                    break;
-                case 1:
-                    set[y][x] = "\033[0;41m";
-                    break;
-                case 2:
-                    set[y][x] = "\033[0;42m";
-                    break;
-                case 3:
-                    set[y][x] = "\033[0;43m";
-                    break;
-                case 4:
-                    set[y][x] = "\033[0;44m";
-                    break;
-                case 5:
-                    set[y][x] = "\033[0;45m";
-                    break;
-                case 6:
-                    set[y][x] = "\033[0;46m";
-                    break;
-                default:
-                    set[y][x] = "\033[0;47m";  // White block (outside)
-                    break;
+        // gen set
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                double real = coord_trnsfrm(x, width, real_min, real_max);
+                double imag = coord_trnsfrm(y, height, imag_min, imag_max);
+
+                unsigned int count = brot_point(real, imag);
+                set[y][x] = color(count);
             }
         }
-    }
 
-    for (int y = height - 1; y >= 0; y--) {
-        for (int x = 0; x < width; x++) {
-            printf("%s \033[0m", set[y][x]);
+        // print set
+        for (int y = height - 1; y >= 0; y--) {
+            for (int x = 0; x < width; x++) {
+                printf("%s \033[0m", set[y][x]);
+            }
+            printf("\n");
         }
-        printf("\n");
+
+        // input
+        printf("hit enter to submit choice:\n");
+        printf("? q - quit, h - left, j - down, k - up, l - right, + - zoom in, - - zoom in\r");
+        char cmd = (char)getchar();
+
+        double real_range = real_max - real_min;
+        double imag_range = imag_max - imag_min;
+
+        double real_change = real_range * .1;
+        double imag_change = imag_range * .1;
+        switch (cmd) {
+            case 'q':
+                return 0;
+                break;
+            case 'j':
+                imag_min -= imag_change;
+                imag_max -= imag_change;
+                break;
+            case 'k':
+                imag_min += imag_change;
+                imag_max += imag_change;
+                break;
+            case 'h':
+                real_min -= real_change;
+                real_max -= real_change;
+                break;
+            case 'l':
+                real_min += real_change;
+                real_max += real_change;
+                break;
+            case '+':
+                real_min += real_change;
+                real_max -= real_change;
+
+                imag_min += imag_change;
+                imag_max -= imag_change;
+                break;
+            case '-':
+                real_min -= real_change;
+                real_max += real_change;
+
+                imag_min -= imag_change;
+                imag_max += imag_change;
+                break;
+        }
     }
 
     return 0;
 }
 
-int brot_point(double x, double y) {
+int brot_point(double real, double imag) {
     double a = 0.0, b = 0.0;
     int count = 0;
 
     while (count < 100) {
-        double a_n = a * a - b * b + x;
-        double b_n = 2 * a * b + y;
+        double a_n = a * a - b * b + real;
+        double b_n = 2 * a * b + imag;
 
         a = a_n;
         b = b_n;
@@ -75,4 +106,40 @@ int brot_point(double x, double y) {
     }
 
     return 0;
+}
+
+double coord_trnsfrm(unsigned int index, unsigned int size, double bot_bound, double up_bound) {
+    double percent = index / (double)(size - 1);
+    double range = up_bound - bot_bound;
+
+    return (percent * range) + bot_bound;
+}
+
+char* color(unsigned int count) {
+    switch (count) {
+        case 0:
+            return "\033[0;40m";  // Black block (in set)
+            break;
+        case 1:
+            return "\033[0;41m";
+            break;
+        case 2:
+            return "\033[0;42m";
+            break;
+        case 3:
+            return "\033[0;43m";
+            break;
+        case 4:
+            return "\033[0;44m";
+            break;
+        case 5:
+            return "\033[0;45m";
+            break;
+        case 6:
+            return "\033[0;46m";
+            break;
+        default:
+            return "\033[0;47m";  // White block (outside)
+            break;
+    }
 }
